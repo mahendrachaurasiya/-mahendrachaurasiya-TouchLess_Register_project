e1bda728ec8e42f94caae696944e04258ec51a04
mahendrachaurasiya <mahendrachaurasiya2011@gmail.com>
2021-05-12 22:07:03 +0530

    touch less attendence

---
+++ AttendenceProject.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path='imagesAttendance'
+images=[]
+classNames=[]
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg=cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList  = []
+    for img in images:
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendance(name):
+    with open('attendenceRegister.csv','r+')as f:
+        myDataList=f.readline()
+        nameList=[]
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
+#markAttendance('Elon')
+
+encodeListKnown = findEncodings(images)
+print('Encoding complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success,img = cap.read()
+    imgs = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgs=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+    facesCurFrame= face_recognition.face_locations(imgs)
+    encodesCurFrame = face_recognition.face_encodings(imgs)
+
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name= classNames[matchIndex].upper()
+           # print(name)
+            y1,x2,y2,x1=faceLoc
+            #y1, x2, y2, x1=y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),3)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+    cv2.imshow('webcam',img)
+    cv2.waitKey(1)
+
+
+
+
+
+#
+# imgElon = face_recognition.load_image_file('imagesAttendance/elon.jpg')
+# imgElon=cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+# imgTest = face_recognition.load_image_file('imagesAttendance/bill.jpg')
+# imgTest=cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
